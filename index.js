a00ef97ca27a86b4711e73d09eb48804d0a69aa3
Gabriel de Carli <154674155+gaabdevelop@users.noreply.github.com>
2024-03-01 11:03:00 -0300

    Update index.js
    Melhoria e diminuição de código

--- index.js
+++ index.js
@@ -11,20 +11,10 @@ const main = async () => {
     const $ = cheerio.load(content);
     let info = [];
 
-    // Extrair informações desejadas
-    const prefeito = $('.resultados-destaque li:contains("Prefeito") .ind-value').text().trim();
-    const areaTerritorial = $('.resultados-padrao li:contains("Área Territorial") .ind-value').text().trim();
-    const populacaoResidente = $('.resultados-padrao li:contains("População residente") .ind-value').text().trim();
-    const densidadeDemografica = $('.resultados-padrao li:contains("Densidade demográfica") .ind-value').text().trim();
-    const escolarizacao = $('.resultados-padrao li:contains("Escolarização 6 a 14 anos") .ind-value').text().trim();
-    const idhm = $('.resultados-padrao li:contains("IDHM") .ind-value').text().trim();
-    const mortalidadeInfantil = $('.resultados-padrao li:contains("Mortalidade infantil") .ind-value').text().trim();
-    const receitasRealizadas = $('.resultados-padrao li:contains("Receitas realizadas") .ind-value').text().trim();
-    const despesasEmpenhadas = $('.resultados-padrao li:contains("Despesas empenhadas") .ind-value').text().trim();
-    const pibPerCapita = $('.resultados-padrao li:contains("PIB per capita") .ind-value').text().trim();
-
-    const data = [prefeito, areaTerritorial, populacaoResidente, densidadeDemografica, escolarizacao, idhm, mortalidadeInfantil, receitasRealizadas, despesasEmpenhadas, pibPerCapita ]
-    info.push(data);
+    $('.resultados-padrao > li').each((index, element) => {
+        const areaTerritorial = $(element).text();
+        info.push([areaTerritorial]);
+    });
 
     console.log(info);
 };
